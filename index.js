c60d41858c0fa0e794e0c7e8e5a50bbd9b5b6e30
Qhris <130830468+Esteemchris10@users.noreply.github.com>
2026-05-01 23:52:28 +0100

    update functionality and interface

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("scoreboard")
+let scoreHome = 0
+let scoreAway = 0
+let scoreElhome = document.getElementById("scoreHome")
+let scoreElaway = document.getElementById("scoreAway")
+
+    scoreElhome.textContent = scoreHome
+    scoreElaway.textContent = scoreAway
+
+// Home functions
+function HomeincrementOne() {
+  scoreHome += 1
+  scoreElhome.textContent = scoreHome
+}
+
+function HomeincrementTwo() {
+  scoreHome += 2
+  scoreElhome.textContent = scoreHome
+}
+function HomeincrementThree() {
+  scoreHome += 3
+  scoreElhome.textContent = scoreHome
+}
+
+// Away functions
+function AwayincrementOne() {
+  scoreAway += 1
+  scoreElaway.textContent = scoreAway
+}
+
+function AwayincrementTwo() {
+  scoreAway += 2
+  scoreElaway.textContent = scoreAway
+}
+function AwayincrementThree() {
+  scoreAway += 3
+  scoreElaway.textContent = scoreAway
+}
